8142d7d79f359264f2eb52891c2e9d23ddb4eba3
catuchowil <gapuentes@hotmail.com.ar>
2022-12-17 09:01:25 -0300

    Registro de modelos en el admin

--- app/api/admin.py
+++ app/api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Categoria, SubCategoria
+
+
+
+
+
+class CategoriaAdmin(admin.ModelAdmin):
+    search_fields = ['descripcion']
+    list_display = ('id','descripcion')
+
+
+class SubCategoriaAdmin(admin.ModelAdmin):
+    search_fields = ['descripcion']
+    list_display = ('id','categoria','descripcion')
+    
+
+
 
 # Register your models here.
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(SubCategoria, SubCategoriaAdmin)
