e8f1a05c34c972d1d661967d97d9a83a00ed8d26
catuchowil <gapuentes@hotmail.com.ar>
2022-12-17 10:37:26 -0300

    Serializadores y urls

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path
 
+from app.api.views import ProductoList, ProductoDetalle, ProductoListado, ProductoCrear
+
 urlpatterns = [
+     # url de borjorge
+    path('borjorge/v1/productos/', ProductoList.as_view(),name='producto_list' ),
+    path('borjorge/v1/productos/<int:pk>', ProductoDetalle.as_view(),name='producto_detalle' ),
+
+    # url de santa cruz
+    path('santacruz/v2/productos/listado', ProductoListado.as_view(),name='productolistado' ),
+    path('santacruz/v2/productos/crear', ProductoCrear.as_view(),name='productocrear' ),
     
 ]
--- app/api/views.py
+++ app/api/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
+#--Realizamos la importación
+
+from rest_framework import generics
+from .models import Producto
+from .serializers import ProductoSerializer
+
 # Create your views here.
+
+#--Nota Borjorge utiliza vistas genéricas que permite crear y listar
+#--Santa Cruz utiliza una para listar ListAPIView y otra para crear CreateAPIView
+
+#--Aqui usamos lo de borjorge
+
+class ProductoList(generics.ListCreateAPIView):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+ 
+ 
+class ProductoDetalle(generics.RetrieveDestroyAPIView):
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+
+
+#--Aqui usamos lo de Santa Cruz
+
+class ProductoListado(generics.ListAPIView):
+    serializer_class = ProductoSerializer
+    def get_queryset(self):
+        return Producto.objects.all()
+
+
+class ProductoCrear(generics.CreateAPIView):
+    serializer_class = ProductoSerializer
+   
